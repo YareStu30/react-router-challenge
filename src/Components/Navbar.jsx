0cd4653d654baec6706b868898d199f9c384debf
YareStu30 <yare.cs22@gmail.com>
2022-09-24 01:31:33 -0300

    add icons and styles

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,14 +3,31 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <h1> Happy Cake</h1>
-      <Link to="/" className="navbar-brand text-uppercase navbar-dark">Home</Link>
-      <Link to="/contact" className="navbar-brand text-uppercase navbar-dark">Contact</Link>
-      <img
-        className="icono"
-        src="https://cdn-icons-png.flaticon.com/512/1784/1784096.png"
-        alt=""
-      />
+      <nav class="navbar navbar-default" role="navigation">
+        <img
+          className="icon1"
+          src="https://cdn-icons-png.flaticon.com/512/8244/8244509.png"
+          alt=""
+        />
+        <Link to="/" className="navbar-brand text-uppercase ">
+          Home
+        </Link>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3158/3158480.png"
+          alt=""
+          className="icon1"
+        />
+        <Link to="/contact" className="navbar-brand text-uppercase ">
+          Contact
+        </Link>
+
+        <h1 className="title"> Happy Cake</h1>
+        <img
+          className="icono"
+          src="https://cdn-icons-png.flaticon.com/512/1784/1784096.png"
+          alt=""
+        />
+      </nav>
     </>
   );
 };
