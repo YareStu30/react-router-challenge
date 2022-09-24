3e49f50bddaada8e4575364a215ec7f23c5fda53
YareStu30 <yare.cs22@gmail.com>
2022-09-24 01:06:01 -0300

    add cols, classname, h1 in contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,17 +4,23 @@ const Contact = () => {
   return (
     <div className="container">
       <form>
+        <h1>Cuentanos, ¿En qué podemos ayudarte?</h1>
         <div>
           <p>Email</p>
-          <input type="email" name="email" id="txtEmail" />
+          <input type="email" name="email" id="txtEmail" cols="50" className="w-50" />
         </div>
 
         <div>
           <p>Descripción</p>
-          <textarea name="description" id="txtDescription" cols="50"></textarea>
+          <textarea
+            name="description"
+            id="txtDescription"
+            cols="50"
+            className="w-50"
+          ></textarea>
         </div>
 
-        <input type="submit" value="Send" id="btnSend" className="btn2" />
+        <input type="submit" value="Send" id="btnSend" className="btn2 " />
       </form>
     </div>
   );
